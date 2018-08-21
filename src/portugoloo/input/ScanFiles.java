fbbb4ff694faeb4481c1878b720a581eaf688d32
Jonatas <jonatas.silvestrini@gmail.com>
2018-08-21 13:30:44 -0300

    Alterações na classe ScanFiles

--- src/portugoloo/input/ScanFiles.java
+++ src/portugoloo/input/ScanFiles.java
@@ -5,10 +5,28 @@
  */
 package portugoloo.input;
 
+import java.io.File;
+
 /**
  *
  * @author JONATASWILLIAMSILVES
  */
-public class ScanFiles { 
+
+public class ScanFiles {
+    private File path;
+    
+    public void setPath(String $path){
+        this.path = new File($path); 
+    }
+    
+    public void Scan(final File $pasta){
+        for (final File listaArquivos : this.path.listFiles()){
+            if (listaArquivos.isDirectory()){
+                Scan(listaArquivos);
+            } else {
+                System.out.println(listaArquivos.getName());
+            }
+        }
+    }
     
 }
