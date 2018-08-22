8e40683e39f6eef14e3fe52efa37b4386d91eafe
Jonatas <jonatas.silvestrini@gmail.com>
2018-08-22 13:23:35 -0300

    Alteralções na classe ScanFiles. Criação da classe Arquivo

--- src/portugoloo/input/ScanFiles.java
+++ src/portugoloo/input/ScanFiles.java
@@ -5,7 +5,15 @@
  */
 package portugoloo.input;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,15 +22,31 @@ import java.io.File;
 public class ScanFiles {
 
 	private File path;
+        private List<Arquivo> arquivos = new ArrayList<Arquivo>();
+        private StringBuilder nomeArq, conteudoArq;
+        private FileReader arq;
+        private String lerArq;
 
 	public void setPath(String $path) {
 		this.path = new File($path);
 	}
 
-	public void Scan() {
+	public void Scan() throws IOException {
 		for (final File listaArquivos : this.path.listFiles()) {
 			if (listaArquivos.getName().endsWith("classe")){
-				
+				this.nomeArq = listaArquivos.getName();
+                            try {
+                                this.arq = new FileReader(listaArquivos.getPath());
+                            } catch (FileNotFoundException ex) {
+                                Logger.getLogger(ScanFiles.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            this.lerArq = new BufferedReader(this.arq);
+                            String conteudoLinha = this.lerArq.readLine();
+                            while (conteudoLinha != null){
+                                this.conteudoArq += conteudoLinha;
+                                this.lerArq.readLine();
+                            }
+                            
 			}
 		}
 	}
