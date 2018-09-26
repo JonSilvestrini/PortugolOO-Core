a2b3856ad0823cd0bfb3364767bf396589c3190f
Jonatas <jonatas.silvestrini@gmail.com>
2018-09-25 22:46:38 -0300

    Alterações Lexicos

--- src/portugoloo/lexico/Converte.java
+++ src/portugoloo/lexico/Converte.java
@@ -5,19 +5,45 @@
  */
 package portugoloo.lexico;
 
+import java.util.ArrayList;
+import java.util.List;
 import javabeans.Arquivo;
+import javabeans.ArquivoIntermediario;
+
 
 /**
  *
  * @author JONATASWILLIAMSILVES
  */
 public class Converte {
-    
-    public void converter(Arquivo arquivos){
-        for (String linha : arquivos.getConteudoArq()) {
-            
-        }
-        
-    }
-    
+
+	private ArquivoIntermediario arqInt;
+	private DicionarioPortugol dicpol;
+	private List<Integer> cod;
+	private List<ArquivoIntermediario> listaInter = new ArrayList<ArquivoIntermediario>();
+
+	public List<ArquivoIntermediario> getListaInter() {
+		return listaInter;
+	}
+
+	public void setListaInter(List<ArquivoIntermediario> listaInter) {
+		this.listaInter = listaInter;
+	}
+
+	
+	
+	public void converter(List<Arquivo> arquivos) {
+		for (Arquivo arquivo : arquivos) {
+			arqInt = new ArquivoIntermediario();
+			cod = new ArrayList<Integer>();
+			arqInt.setNomeInt(arquivo.getNomeArq());
+			for (String linha : arquivo.getConteudoArq()) {
+				System.out.println(linha);
+				dicpol.setPalavra(linha);
+				cod.add(dicpol.consultaPalavra());
+			}
+			arqInt.setConteudoInt(cod);
+			this.listaInter.add(arqInt);
+		}
+	}
 }
--- src/portugoloo/lexico/DicionarioPortugol.java
+++ src/portugoloo/lexico/DicionarioPortugol.java
@@ -10,114 +10,114 @@ package portugoloo.lexico;
  * @author JONATASWILLIAMSILVES
  */
 public class DicionarioPortugol {
-    
-    private String palavra;
-    private int token;
-    
-    public void setPalavra(String $palavra){
-        this.palavra = $palavra;
-    }
-    
-    
-    public int consultaPalavra(){
-        switch(this.palavra){
-            case "publica":
-                this.token = 0;
-                break;
-            
-            case "publico":
-                this.token = 0;
-                break;
-            
-            case "classe":
-                this.token = 1;
-                break;
-                
-            case "metodo":
-                this.token = 2;
-                break;
-                
-            case "Principal()":
-                this.token = 3;
-                break;
-                
-            case "escreva":
-                this.token = 4;
-                break;
-               
-            case "escrevaln":
-                this.token = 5;
-                break;
-                
-            case "texto":
-                this.token = 6;
-                break;
-                
-            case "inteiro":
-                this.token = 7;
-                break;
-            
-            case "real":
-                this.token = 8;
-                break;
-                
-            case "caractere":
-                this.token = 9;
-                break;
-                
-            case "logico":
-                this.token = 10;
-                break;
-                
-            case "verdadeiro":
-                this.token = 11;
-                break;
-                
-            case "falso":
-                this.token = 12;
-                break;
-                
-            case "nulo":
-                this.token = 13;
-                break;
-                
-            case "vazio":
-                this.token = 14;
-                break;
-                
-            case "caso":
-                this.token = 15;
-                break;
-                
-            case "escolha" :
-                this.token = 16;
-                break;
-                
-            case "para" :
-                this.token = 17;
-                break;
-                
-            case "enquanto":
-                this.token = 18;
-                break;
-                
-            case "faça":
-                this.token = 19;
-                break;
-                
-            case "estatico":
-                this.token = 20;
-                break;
-                
-            case "constante":
-                this.token = 21;
-                break;
-                
-            default:
-                break;
-        }
-        
-        return this.token;
-    }
-    
+
+	private String palavra;
+	private int token;
+
+	public void setPalavra(String $palavra) {
+		this.palavra = $palavra;
+	}
+
+	public int consultaPalavra() {
+		switch (this.palavra) {
+			case "publica":
+				this.token = 0;
+				break;
+
+			case "publico":
+				this.token = 0;
+				break;
+
+			case "classe":
+				this.token = 1;
+				break;
+
+			case "metodo":
+				this.token = 2;
+				break;
+
+			case "Principal()":
+				this.token = 3;
+				break;
+
+			case "escreva":
+				this.token = 4;
+				break;
+
+			case "escrevaln":
+				this.token = 5;
+				break;
+
+			case "texto":
+				this.token = 6;
+				break;
+
+			case "inteiro":
+				this.token = 7;
+				break;
+
+			case "real":
+				this.token = 8;
+				break;
+
+			case "caractere":
+				this.token = 9;
+				break;
+
+			case "logico":
+				this.token = 10;
+				break;
+
+			case "verdadeiro":
+				this.token = 11;
+				break;
+
+			case "falso":
+				this.token = 12;
+				break;
+
+			case "nulo":
+				this.token = 13;
+				break;
+
+			case "vazio":
+				this.token = 14;
+				break;
+
+			case "caso":
+				this.token = 15;
+				break;
+
+			case "escolha":
+				this.token = 16;
+				break;
+
+			case "para":
+				this.token = 17;
+				break;
+
+			case "enquanto":
+				this.token = 18;
+				break;
+
+			case "faça":
+				this.token = 19;
+				break;
+
+			case "estatico":
+				this.token = 20;
+				break;
+
+			case "constante":
+				this.token = 21;
+				break;
+
+			default:
+				this.token = 99999999;
+				break;
+		}
+
+		return this.token;
+	}
+
 }
