6e9bf865a1c122b1d73cbc9055f6f65e0e516972
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-10-26 15:25:28 -0200

    Classe Compilador modificado para informar output da programação em tempo real

--- src/portugoloo/interpretador/Compilador.java
+++ src/portugoloo/interpretador/Compilador.java
@@ -5,6 +5,8 @@
  */
 package portugoloo.interpretador;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -25,13 +27,28 @@ public class Compilador {
 	private List<Arquivo> arquivos;
 
 	private String path;
+
+	private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 	
 	private String OutputText;
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		support.addPropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		support.removePropertyChangeListener(listener);
+	}
 
 	public String getOutputText() {
 		return OutputText;
 	}
 
+	private void setOutputText(String text) {
+		String oldValue = this.OutputText;
+		this.OutputText = text;
+		support.firePropertyChange("OutputText", oldValue, text);
+	}
 	
 
 	public Compilador(List<Arquivo> arquivos, String path) {
@@ -39,8 +56,28 @@ public class Compilador {
 		this.path = path + "/output/";
 	}
 
+	public Compilador() {
 
-	public void Compile() throws Exception{
+	}
+
+	public List<Arquivo> getArquivos() {
+		return arquivos;
+	}
+
+	public void setArquivos(List<Arquivo> arquivos) {
+		this.arquivos = arquivos;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path + "/output/";
+	}
+
+
+	public boolean Compile() throws Exception{
 		
 		for (final Arquivo arq : arquivos) {
 			if (arq.isMain()) {
@@ -49,10 +86,23 @@ public class Compilador {
 			Writer(arq);
 		}
 
-		runProcess("javac -classpath " + this.path + " " + this.path  + arquivoMain.getNomeArq() + ".java");
-		
-		runProcess("java -classpath " + this.path + " " + arquivoMain.getNomeArq());
+		if (runProcess("javac -classpath " + this.path + " " + this.path  + arquivoMain.getNomeArq() + ".java") !=0)
+			return false;
 
+
+		
+		if (runProcess("java -classpath " + this.path + " " + arquivoMain.getNomeArq()) != 0 )
+			return false;
+
+		return true;
+	}
+
+	public boolean CleanCompile() throws Exception {
+		File index = new File(this.path);
+		for (final File file : index.listFiles()){
+			file.delete();
+		}
+		return this.Compile();
 	}
 
 
@@ -62,22 +112,23 @@ public class Compilador {
 			new InputStreamReader(ins));
 		while ((line = in.readLine()) != null) {
 			System.out.println(name + " " + line);
-			OutputText+= line + "\n";
+			setOutputText(line);
 		}
 	}
 
-	private void runProcess(String command) throws Exception {
+	private int runProcess(String command) throws Exception {
 		Process pro = Runtime.getRuntime().exec(command);
 
 		printLines(arquivoMain.getNomeArq()+" stdout:", pro.getInputStream());
 		printLines(arquivoMain.getNomeArq()+" stderr:", pro.getErrorStream());
 		pro.waitFor();
 		System.out.println(arquivoMain.getNomeArq()+" exitValue() " + pro.exitValue());
+		return pro.exitValue();
 	}
 	
 
 	private void Writer (Arquivo arq) {
-		new File(this.path).delete();
+		
 		try {
 			new File(path).mkdirs();
 			PrintWriter w = new PrintWriter(this.path + arq.getNomeArq() + ".java", "UTF-8");
